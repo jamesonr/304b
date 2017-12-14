2bbd2ae7b4c3c17e51d1e71e987ce751f2d3ce0b
Robert Jameson <jamesonr@uni.coventry.ac.uk>
2017-12-14 02:54:38 +0000

    Login fully working - user name now displaying top right when logged in user now has encryption token using jwt - only non sensitive data of user showing in console i.e. username email

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,7 @@
 var User = require('../models/user');
-
-module.exports=function(router){
+var jwt = require('jsonwebtoken');
+var secret = 'jadore';
+module.exports = function(router) {
   //User Registration Route
   router.post('/users', function(req, res) {
     var user = new User();
@@ -11,49 +12,97 @@ module.exports=function(router){
     user.password = req.body.password;
     user.email = req.body.email;
     if (user.title == null || user.title == '' || user.firstname == null || user.firstname == '' || user.surname == null || user.surname == '' || user.username == null || user.username == '' || user.password == null || user.password == '' || user.email == null || user.email == '') {
-      res.json({success:false, message:'Ensure all fields were provided'});
+      res.json({
+        success: false,
+        message: 'Ensure all fields were provided'
+      });
     } else {
       user.save(function(err) {
         if (err) {
-          res.json({ success:false, message:'Username or Email already exists' });
+          res.json({
+            success: false,
+            message: 'Username or Email already exists'
+          });
         } else {
-          res.json({ success:true, message:'User Created' });
+          res.json({
+            success: true,
+            message: 'User Created'
+          });
         }
       });
     }
   });
 
   //User Login Route
-  router.post('/authenticate', function (req, res) {
+  router.post('/authenticate', function(req, res) {
 
     var promptedPassword = req.body.password;
     var promptedUsername = req.body.username;
 
 
-        User.findOne({ username: promptedUsername }).select('email username password').exec(function (err, user) {
-            if (!user) {
-                res.json({
-                    success: false,
-                    message: 'Wrong username or password. Please try again.'
-                });
-            } else if (user) {
-                var validPassword = user.passwordValidation(promptedPassword);
-                if (validPassword) {
-                  res.json({
-                      success: true,
-                      message: 'Authenticated!'
-                  });
-                } else {
-                  res.json({
-                      success: false,
-                      message: 'Wrong password. Please try again. ' + promptedPassword + '- password ' + promptedUsername + '- username' + '. Return from userSchema: ' + user.passwordValidation(promptedPassword)
-                  });
-                }
+    User.findOne({
+      username: promptedUsername
+    }).select('email username password').exec(function(err, user) {
+        if (!user) {
+          res.json({
+            success: false,
+            message: 'Wrong username or password. Please try again.'
+          });
+        } else if (user) {
+          var validPassword = user.passwordValidation(promptedPassword);
+          if (validPassword) {
+            var token = jwt.sign({
+              username: user.username,
+              email: user.email
+            }, secret, {
+              expiresIn: '24h'
+            }); //after 24hours webtoken expires/no longer usable - token is stored on userside cache
+            res.json({
+              success: true,
+              message: 'Authenticated!',
+              token: token
+            });
+          } else {
+            res.json({
+              success: false,
+              message: 'Wrong password. Please try again.'
+            });
+          }
+        }
+      })
+    });
 
-            }
-        })
-    })
 
-return router;
+  //middleware
+  router.use(function(req, res, next) {
 
+    var token = req.body.token || req.body.query || req.headers['x-access-token'];
+
+    if (token) {
+      //verify token
+      jwt.verify(token, secret, function(err, decoded) {
+        if (err) {
+          res.json({
+            success: false,
+            message: 'Token Invalid'
+          });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      res.json({
+        success: false,
+        message: 'No token provided'
+      });
+    }
+
+  });
+
+  router.post('/me', function(req, res) {
+    res.send(req.decoded);
+  });
+
+  return router;
 };
